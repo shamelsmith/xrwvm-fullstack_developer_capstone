349a4c127530c8ade4dda054a8386cf51e23cfb8
shamelsmith <shamelsmith@gmail.com>
2024-09-24 15:54:44 -0400

    [FIX] Linting

--- server/database/app.js
+++ server/database/app.js
@@ -1,4 +1,6 @@
 /*jshint esversion: 6 */
+/*jshint sub:true*/
+/* experimental: [asyncawait] */
 const express = require("express");
 const mongoose = require("mongoose");
 const fs = require("fs");
