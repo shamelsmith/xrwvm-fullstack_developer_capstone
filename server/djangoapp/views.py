d3484a163ce8b72d94c26e1484e21f2494ad3e92
Stephen <shamelsmith@gmail.com>
2024-09-24 20:56:50 -0400

    Containerize your Application

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,8 +18,7 @@ from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
 from .models import CarMake, CarModel
 
-# from .restapis import get_request, analyze_review_sentiments, post_review
-from .restapis import get_request, post_review
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
@@ -107,6 +106,10 @@ def get_dealer_reviews(request, dealer_id):
     if dealer_id:
         endpoint = "/fetchReviews/dealer/" + str(dealer_id)
         reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
         return JsonResponse({"status": 200, "reviews": reviews})
     else:
         return JsonResponse({"status": 400, "message": "Bad Request"})
